7aac5c2dee3e0f8c47dd94111e31df7424fb9238
SemihaOZCAN <ozcanfertel@gmail.com>
2024-05-08 19:13:36 +0300

    frm isafir kart updated

--- OtelRezervasyon/Formlar/Misafir/FrmMisafirKart.cs
+++ OtelRezervasyon/Formlar/Misafir/FrmMisafirKart.cs
@@ -17,6 +17,7 @@ namespace OtelRezervasyon.Formlar.Misafir
     {
         public FrmMisafirKart()
         {
+            //TODO
             InitializeComponent();
         }
         DbOtelRezervasyonEntities3 db=new DbOtelRezervasyonEntities3();
